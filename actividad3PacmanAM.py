b240c729046eb68cd7ed4008008bc233b6a5d9ac
AndyMurilloJimenez <A01655932@tec.mx>
2025-10-31 13:32:53 -0600

    Modifiqué el tablero y la velocidad de los fantasmas jajs 🌀

---
+++ actividad3PacmanAM.py
@@ -0,0 +1,164 @@
+"""Pacman con fantasmas nivel 1 (movimiento parcialmente aleatorio)."""
+
+from random import choice
+from turtle import *
+from freegames import floor, vector
+
+"""CAMBIOS GAEL WASO, NIVEL 1 DE INTELIGENCIA, MOVIMIENTO PARCIALMENTE AL AZAR DE LOS FANTASMAS"""
+inteligencia = 1  
+
+state = {'score': 0}
+path = Turtle(visible=False)
+writer = Turtle(visible=False)
+aim = vector(5, 0)
+pacman = vector(-40, -80)
+
+
+ghosts = [
+    [vector(-180, 160), vector(19, 0)],
+    [vector(-180, -160), vector(0, 19)],
+    [vector(100, 160), vector(0, -19)],
+    [vector(100, -160), vector(-19, 0)],
+]
+
+""" modiifcaciones de andy: el tablero y aumentarle la velocidad a los fantasmas. Hospedador: Gael"""
+tiles = [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0,
+    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+    0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
+    0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0,
+    0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0,
+    0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0,
+    0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0,
+    0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+]
+
+
+def square(x, y):
+    path.up()
+    path.goto(x, y)
+    path.down()
+    path.begin_fill()
+    for count in range(4):
+        path.forward(20)
+        path.left(90)
+    path.end_fill()
+
+
+def offset(point):
+    x = (floor(point.x, 20) + 200) / 20
+    y = (180 - floor(point.y, 20)) / 20
+    return int(x + y * 20)
+
+
+def valid(point):
+    index = offset(point)
+    if tiles[index] == 0:
+        return False
+    index = offset(point + 19)
+    if tiles[index] == 0:
+        return False
+    return point.x % 20 == 0 or point.y % 20 == 0
+
+
+def world():
+    bgcolor('black')
+    path.color('blue')
+    for index, tile in enumerate(tiles):
+        if tile > 0:
+            x = (index % 20) * 20 - 200
+            y = 180 - (index // 20) * 20
+            square(x, y)
+            if tile == 1:
+                path.up()
+                path.goto(x + 10, y + 10)
+                path.dot(2, 'white')
+
+
+def move():
+    writer.undo()
+    writer.write(state['score'])
+    clear()
+
+    
+    if valid(pacman + aim):
+        pacman.move(aim)
+
+    index = offset(pacman)
+    if tiles[index] == 1:
+        tiles[index] = 2
+        state['score'] += 1
+        x = (index % 20) * 20 - 200
+        y = 180 - (index // 20) * 20
+        square(x, y)
+
+    up()
+    goto(pacman.x + 10, pacman.y + 10)
+    dot(20, 'yellow')
+
+    """CAMBIOS GAEL WASO, MOVIMIENTO DE FANTASMAS"""
+    for point, course in ghosts:
+        if valid(point + course):
+            point.move(course)
+        else:
+            options = [vector(5, 0), vector(-5, 0),
+                       vector(0, 5), vector(0, -5)]
+            valid_moves = [v for v in options if valid(point + v)]
+
+            """CAMBIOS GAEL WASO, SE ELIGEN ENTRE LAS DOS DIRECCIONES MAS CERCANAS A PAC-MAN"""
+            if valid_moves:
+                valid_moves.sort(key=lambda v: abs((point + v) - pacman))
+                plan = choice(valid_moves[:2])
+            else:
+                plan = choice(options)
+
+            course.x, course.y = plan.x, plan.y
+            point.move(course)
+
+        up()
+        goto(point.x + 10, point.y + 10)
+        dot(20, 'red')
+
+    update()
+
+    
+    for point, course in ghosts:
+        if abs(pacman - point) < 20:
+            return
+
+    ontimer(move, 100)
+
+
+def change(x, y):
+    if valid(pacman + vector(x, y)):
+        aim.x, aim.y = x, y
+
+
+
+setup(420, 420, 370, 0)
+hideturtle()
+tracer(False)
+writer.goto(160, 160)
+writer.color('white')
+writer.write(state['score'])
+listen()
+onkey(lambda: change(5, 0), 'Right')
+onkey(lambda: change(-5, 0), 'Left')
+onkey(lambda: change(0, 5), 'Up')
+onkey(lambda: change(0, -5), 'Down')
+world()
+move()
+done()
+
